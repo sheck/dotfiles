fe3501cdca20d676b97a1cf45147ea58fcf73997
Nick Sheck <sheck@users.noreply.github.com>
2017-08-10 15:01:23 -0700

    Completions case insensitive

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -4,3 +4,6 @@ fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 # completion
 autoload -U compinit
 compinit
+
+# make completions case-insensitive
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
