45c0161196304fc91484f2b948eadeb3077fdd80
Nick Sheck <sheck@users.noreply.github.com>
2017-08-10 14:35:11 -0700

    Git dirty branch marker

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,8 +1,11 @@
 # modify the prompt to contain git branch name if applicable
+dirty_branch_marker() {
+  [[ $(git status --porcelain 2> /dev/null) ]] && echo " %{$fg_bold[red]%}✗%{$reset_color%}"
+}
 git_prompt_info() {
   current_branch=$(git current-branch 2> /dev/null)
   if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}$(dirty_branch_marker)"
   fi
 }
 setopt promptsubst
